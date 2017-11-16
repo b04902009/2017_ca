8659d3cecf2f29c40d880a17464fb3dd024b7b61
Chian-Huei Hsiao <b04902009@ntu.edu.tw>
2017-11-16 13:07:37 +0800

    v0

--- hw4/code/compile.sh
+++ hw4/code/compile.sh
@@ -1,15 +1,2 @@
-iverilog -o hw4 \
-Adder.v \
-ALU.v \
-ALU_Control.v \
-Control.v \
-CPU.v \
-Instruction_Memory.v \
-MUX5.v \
-MUX32.v \
-PC.v \
-Registers.v \
-Sign_Extend.v \
-testbench.v
-
+iverilog -o hw4 *.v
 vvp hw4
